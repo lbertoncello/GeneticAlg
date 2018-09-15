139f77628e7f02a9c4ebcc262da0066d4fac1122
natanaeltnn <43181609+natanaeltnn@users.noreply.github.com>
2018-09-15 13:51:58 -0300

    Update objetiva.py

--- objetiva.py
+++ objetiva.py
@@ -6,7 +6,7 @@ def fobj(P, M):
     s = 0
     c_ant = P[0]
     for c in P[1:]:
-        s+=M[c_ant][c]
+        s+=M[c_ant-1][c-1]
         c_ant = c
-    s+=M[P[-1]][P[0]]
+    s+=M[P[-1]-1][P[0]-1]
     return s
