3a7ee2c271012f06e929277049ff02e6f8c72316
Gabriel Zonatelle <gabrielzonatelle@gmail.com>
2018-09-25 22:10:49 -0300

    modificacao no sensibilidade.py

--- sensibilidade.py
+++ sensibilidade.py
@@ -1,20 +1,37 @@
-def sensibilidade(lista_params,matrix_dist,v_inicial):
+from genetic_algorithm import *
+import utils as utils
+
+def sensibilidade(lista_params,lista_cidades,v_inicial):
     num_individuos,num_geracoes,taxa_mutacao = lista_params
     melhores_param = [-1,-1,-1]
     melhor_fitness = 999999999
+    distancias = matrix.calc_distances(lista_cidades)
+
+    indice_vertice_inicial = utils.search_vertex_index(v_inicial, lista_cidades)
 
     for ind in num_individuos:
         for ger in num_geracoes:
             for tx in taxa_mutacao:
                 ag = AlgoritmoGenetico(ind)
-                solucao = ag.resolver(tx, ger, matrix_dist, v_inicial)
+                _,solucao = ag.resolver(tx, ger, distancias, indice_vertice_inicial)
 
-                if solucao.distancia_percorrida < melhor_fitness:
-                    melhor_fitness = solucao.distancia_percorrida
+                if solucao < melhor_fitness:
+                    melhor_fitness = solucao
                     melhores_param[0] = ind
                     melhores_param[1] = ger
                     melhores_param[2] = tx
 
                 del ag
 
-    return melhores_param
+    return (melhores_param,melhor_fitness)
+
+
+arq = "att532.tsp"
+l_params = ([50,150,200],[100,200,300],[0.2,0.5,0.7])
+lista_cidades = []
+lista_cidades = getListaCidades(lerEntrada(arq))
+print(lista_cidades)
+params,fit = sensibilidade(l_params,lista_cidades,(7810,6053))
+
+print(params)
+print(fit)
